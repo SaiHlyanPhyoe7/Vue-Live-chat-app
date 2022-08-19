3fe4d738094838dd37ddc6109754ef56f06fd850
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2022-08-19 17:25:37 +0800

    live chat app done

---
+++ src/components/NewChatForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <form>
+      <textarea
+          placeholder="text message and hit enter to send"
+          v-model="message"
+       @keypress.enter="handleSubmit"
+      ></textarea>
+    </form>
+  </div>
+</template>
+
+<script>
+import {ref} from "vue";
+import getUser from "@/composables/getUser";
+import {timestamp} from "@/firebase/config";
+import useCollection from "@/composables/useCollection";
+
+export default {
+setup(){
+  let message= ref("");
+  let {error,addDoc} =useCollection("messages");
+  let {user}=getUser();
+  let handleSubmit=async () => {
+
+    let chat = {
+      message: message.value,
+      name: user.value.displayName,
+      created_at: timestamp()
+    }
+    await addDoc(chat);
+    console.log(chat);
+    message.value = "";
+  }
+
+  return {message,handleSubmit};
+}
+}
+</script>
+
+<style>
+form{
+  margin: 10px;
+}
+textarea{
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 6px;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 0;
+  border-radius: 20px;
+  font-family: inherit;
+  outline: none;
+}
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="container">
     <Navbar></Navbar>
+    <chat-window></chat-window>
+    <NewChatForm></NewChatForm>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
+import {watch} from "vue";
+import getUser from "@/composables/getUser";
+import {useRouter} from "vue-router";
+import NewChatForm from "@/components/NewChatForm";
+import ChatWindow from "@/components/ChatWindow";
 export default {
 components:{
+  ChatWindow,
+  NewChatForm,
   Navbar
-}
+},
+  setup(){
+  let {user}=getUser();
+  let router=useRouter()
+
+    watch(user,()=>{
+    if(!user.value){
+      router.push("/");
+    }
+  })
+  }
 }
 </script>
 
